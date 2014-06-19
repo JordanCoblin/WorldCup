9c2990e1dbbc76f0a4e249e0280770a9754ef904
JordanCoblin <j_coblin@hotmail.com>
2014-06-18 17:32:05 -0700

    implemented slideshow button and updated team standings

--- slide.js
+++ slide.js
@@ -3,46 +3,44 @@ $(document).ready( function() {
 	var slides = $('.slide');
 	var slidesLength = slides.length;
 	var curPosition = 0;
-
+	
+	
 	isVisible();
 	slides.wrapAll('<div id="slidesHolder"></div>');
 	slides.css({'float' : 'left'});
-	$('#slidesHolder').css('width', slideWidth * slidesLength);
+	$('#slidesHolder').css('width', width * slidesLength);
 	
 	function isVisible() {
 		if (curPosition === 0) {
 			$('#previous').hide();
-		} else if (curPosition === slidesLength - 1) {
+		} else { $('#previous').show();}
+		
+		if (curPosition === (slidesLength - 1)) {
 			$('#next').hide();
-		} else {
-			$('#next').show();
-			$('#previous').show();
-			}
+		} else { $('#next').show(); }
 	}
 	
-	function nextSlide() {
-		curPosition++;
-		$('#slidesHolder').animate( {'marginLeft' : (-curPosition) * width} );
-		isVisible();
-	}
-	
-	function previousSlide() {
-		curPosition--;
-		$('#slidesHolder').animate( {'marginLeft' : (-curPosition) * width} );
-		isVisible();
-	}
-	/*
-	function nextPosition() {
+	$('#next').click( function() {
 		if(curPosition < (slidesLength - 1)) {
 			curPosition ++;
 		} else {
 			curPosition = 0;
 		}
 		moveSlide();
-	}
+	});
+	
+	$('#previous').click( function() {
+		if(curPosition > 0) {
+			curPosition --;
+		} else {
+			curPosition = (slidesLength - 1);
+		}
+		moveSlide();
+	});
 					
 	function moveSlide() {
 		$('#slidesHolder').animate( {'marginLeft' : (-curPosition) * width} );
-	}*/
+		isVisible();
+	}
 					
 });
